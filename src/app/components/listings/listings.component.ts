1ddcb08b54f4137ce3d9243d31834357cda651a3
Leo Martinez <l3omtz@gmail.com>
2017-04-06 01:09:56 -0500

    display listings

--- src/app/components/listings/listings.component.ts
+++ src/app/components/listings/listings.component.ts
@@ -18,8 +18,8 @@ export class ListingsComponent implements OnInit {
   ngOnInit() {
     // Call get listings function on initialize
     this._fire.getListings().subscribe(listings => { // <-- Subscribe to it with return of listings
-      this.listings = listings; // <-- Set listings = to the return listings
       console.log(listings);
+      this.listings = listings; // <-- Set listings = to the return listings
     })
   }
 
