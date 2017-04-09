b188003ae5a9d6f24baf9bce3f53e90ad07dbc84
Leo Martinez <l3omtz@gmail.com>
2017-04-09 01:00:21 -0500

    build add listing form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+// Froms
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 // Services
 import { FirebaseService } from './services/firebase.service';
@@ -50,6 +53,7 @@ const myFirebaseAuthConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig), // <-- Import the config and authentication
     routing
   ],
--- src/app/components/addlisting/addlisting.component.ts
+++ src/app/components/addlisting/addlisting.component.ts
@@ -1,10 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'add-listing',
-  templateUrl: './addlisting.html'
+  templateUrl: './addlisting.html',
+  styleUrls: ['./style.css']
 })
 
-export class AddListingComponent {
+export class AddListingComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      title: [, Validators.required],
+      city: [, Validators.required],
+      owner: [, Validators.required],
+      type: [, Validators.required],
+      bedrooms: [, Validators.required],
+      price: [, Validators.required]
+    })
+  }
+
+  ngOnInit() {
+
+  }
 
 }
